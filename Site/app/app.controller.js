9c0a5827c889e046efbaad750f3a455329e39117
Maciej Krzysiak <krzysiak.maciek@gmail.com>
2016-10-25 21:42:39 +0200

    scroll animation :D

--- Site/app/app.controller.js
+++ Site/app/app.controller.js
@@ -1,1 +1,10 @@
-appModule.controller('maniController',[]);
+appModule.controller('mainController', ['$scope', function($scope) {
+  $scope.scrollAnimation = function(obj) {
+    obj.preventDefault();
+    var goto = angular.element(obj.currentTarget).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(goto).offset().top
+    }, 500);
+  };
+}]);
--- Site/assets/js/main.js
+++ Site/assets/js/main.js
@@ -1,18 +1,21 @@
 /**
  * Listen to scroll to change header opacity class
  */
-function checkScroll(){
-    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+// function checkScroll(){
+//     var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+//
+//     if($(window).scrollTop() > startY){
+//         $('.navbar').addClass("scrolled");
+//     }else{
+//         $('.navbar').removeClass("scrolled");
+//     }
+// }
+//
+// if($('.navbar').length > 0){
+//     $(window).on("scroll load resize", function(){
+//         checkScroll();
+//     });
+// }
 
-    if($(window).scrollTop() > startY){
-        $('.navbar').addClass("scrolled");
-    }else{
-        $('.navbar').removeClass("scrolled");
-    }
-}
-
-if($('.navbar').length > 0){
-    $(window).on("scroll load resize", function(){
-        checkScroll();
-    });
-}
+$(document).ready(function() {
+});
