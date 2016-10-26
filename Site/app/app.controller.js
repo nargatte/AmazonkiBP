94702b8d182affc7b494ea5da74a8e7f7ac46e63
Maciej Krzysiak <krzysiak.maciek@gmail.com>
2016-10-27 00:10:48 +0200

    navbar scroll detection

--- Site/app/app.controller.js
+++ Site/app/app.controller.js
@@ -1,10 +1,25 @@
 appModule.controller('mainController', ['$scope', function($scope) {
-  $scope.scrollAnimation = function(obj) {
-    obj.preventDefault();
-    var goto = angular.element(obj.currentTarget).attr('href');
+        $scope.scrollAnimation = function(obj) {
+            obj.preventDefault();
+            var goto = angular.element(obj.currentTarget).attr('href');
 
-    $('html, body').animate({
-        scrollTop: $(goto).offset().top
-    }, 500);
-  };
-}]);
+            $('html, body').animate({
+                scrollTop: $(goto).offset().top
+            }, 500);
+        };
+
+    }])
+    .directive("scroll", function($window) {
+        return function(scope, element, attrs) {
+            angular.element($window).bind("scroll", function() {
+                if (this.pageYOffset >= 50) {
+                    console.log('Scrolled below header.');
+                    // add navbar class with background
+                } else {
+                    console.log('Header is in view.');
+                    // remove navbar class
+                }
+                scope.$apply();
+            });
+        };
+    });
