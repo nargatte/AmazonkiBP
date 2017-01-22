87cf764d3357ea503574ebf3d98d8a4c7a9fcfc8
Maciej Krzysiak <maciej.krzysiak@zoho.com>
2017-01-22 19:46:00 +0100

    header animation

--- Site/app/app.controller.js
+++ Site/app/app.controller.js
@@ -1,4 +1,4 @@
-appModule.controller('mainController', ['$scope', function($scope) {
+appModule.controller('mainController', ['$scope', '$rootScope', function ($scope, $rootScope) {
         $scope.scrollAnimation = function(obj) {
             obj.preventDefault();
             var goto = angular.element(obj.currentTarget).attr('href');
@@ -14,12 +14,27 @@ appModule.controller('mainController', ['$scope', function($scope) {
             angular.element($window).bind("scroll", function() {
                 if (this.pageYOffset >= 50) {
                     console.log('Scrolled below header.');
+
+                    scope.headerAnimation = true;
+
                     var myEl = angular.element(document.querySelector('#navBar'));
-                    myEl.addClass('navbar-amazonki-animation').removeClass('navbar-amazonki');
+                    myEl.removeClass('navbar-amazonki');
+                    setTimeout(function () {
+                        myEl.addClass('navbar-amazonki-animation');
+                    }, 0.001);
+                    
                 } else {
                     console.log('Header is in view.');
+
+                    scope.headerAnimation = false;
+
                     var myEl = angular.element(document.querySelector('#navBar'));
-                    myEl.addClass('navbar-amazonki').removeClass('navbar-amazonki-animation');
+
+                    myEl.removeClass('navbar-amazonki-animation');
+                    setTimeout(function () {
+                        myEl.addClass('navbar-amazonki');
+                    }, 0.001);
+                    
                 }
                 scope.$apply();
             });
